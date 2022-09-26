e48c3631fa5ccdcd95ecc296949d30f1e77bede9
M4asl <mateusz.maslowiec98@gmail.com>
2022-09-26 10:50:00 +0200

    Create nav component

---
+++ components/Nav.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from 'next/link';
+import { FiShoppingBag } from 'react-icons/fi';
+import { NavItems, NavStyles } from '../styles/NavStyles';
+
+const Nav = () => {
+  return (
+    <NavStyles>
+      <Link href={'/'}>Home.</Link>
+      <NavItems>
+        <div>
+          <FiShoppingBag />
+          <h3>Cart</h3>
+        </div>
+      </NavItems>
+    </NavStyles>
+  );
+};
+
+export default Nav;
