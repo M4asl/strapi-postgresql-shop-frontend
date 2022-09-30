e46ea1fad455392fccfa60ac9ae55bcff24906c3
M4asl <mateusz.maslowiec98@gmail.com>
2022-09-30 20:30:14 +0200

    Add total price & total quantity context & animations

--- components/Nav.js
+++ components/Nav.js
@@ -4,20 +4,23 @@ import { FiShoppingBag } from 'react-icons/fi';
 import { NavItems, NavStyles } from '../styles/NavStyles';
 import Cart from './Cart';
 import { useStateContext } from '../lib/context';
+const { AnimatePresence } = require('framer-motion');
 
 const Nav = () => {
-  const { showCart, setShowCart } = useStateContext();
+  const { showCart, setShowCart, totalQuantities } =
+    useStateContext();
   return (
     <NavStyles>
       <Link href={'/'}>Home.</Link>
       <NavItems>
-        <div onClick={() => setShowCart(!showCart)}>
+        <div onClick={() => setShowCart(true)}>
+          {totalQuantities > 0 && <span>{totalQuantities}</span>}
           <FiShoppingBag />
 
           <h3>Cart:)</h3>
         </div>
       </NavItems>
-      {showCart && <Cart />}
+      <AnimatePresence>{showCart && <Cart />}</AnimatePresence>
     </NavStyles>
   );
 };
